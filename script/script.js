bad70aa9a5c87d484b0c2182f34fe415683c48d0
jmiguelmorralla <jmiguelmorralla@gmail.com>
2022-11-26 11:45:33 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -6,16 +6,16 @@ let credito = 20000;
 const precioBalancin = 5000;
 const precioArcoiris = 3000;
 const precioCasita = 2500;
-// let cantidadBalancin = 0;
-// let cantidadArcoiris = 0;
-// let cantidadCasita = 0;
-let stockBalancin = 10
-
+const alicuota = 0.21
 let compras = 0
 
+function totalComprado(creditoInicial, creditoFinal) {
+    console.log(20000 - credito)
+}
 
-const iva = x => total * 0.21
-const neto = (suma, iva) => suma - iva 
+const iva = totalComprado => console.log(totalComprado * 0.21)
+
+
 
 if (responsableInscripto == "si") {
     console.log(`Hola ${nombre}, emitiremos factura tipo A con IVA discriminado. Tu crédito actual es de $${credito}.`)
@@ -23,14 +23,6 @@ if (responsableInscripto == "si") {
     console.log(`Hola ${nombre}, emitiremos factura tipo B. Tu crédito actual es de $${credito}.`)
 } 
 
-
-// while (stockBalancin>=0) {
-//     let compra = parseInt(prompt(`Indique cantidad Balancines que desea comprar. El stock actual es de ${stockBalancin}.`));
-//     if (!stockBalancin) {break;}
-//     stockBalancin -= compra
-// }
-
-// console.log(stockBalancin)
 
 while (credito>=2500) {
     let producto = prompt(`¿Qué producto te gustaría comprar? Podés elegir: Balancin ($${precioBalancin}), Arcoiris ($${precioArcoiris}) o Casita ($${precioCasita}). Los precios incluyen IVA. Tu crédito actual es de $${credito}. Podés escribir ESC para terminar.`)
@@ -47,50 +39,19 @@ while (credito>=2500) {
             break;
 
         default:
-            console.log(`No elegiste ninguna opción.`)
             break;
     }
     
+    if (producto == "" || producto == "ESC" || producto == "esc") {
+        break;
+    }
 }
 
-alert(`Tu crédito es de $${credito}, no te alcanza para ningún producto. Gracias por tus compras.`)
-
-// let seleccionProductos = prompt("Ingrese el nombre del producto que desea comprar de la siguiente lista: Balancin, Arcoiris, Rompecabezas, Casita");
-
-// for (seleccionProductos != "ESC" || seleccionProductos != "esc" || seleccionProductos != ""; seleccionProductos == ("esc"||"ESC" || "");) {
-//     switch (seleccionProductos) {
-//         case "Balancin":
-//             total = precioBalancin;
-//             break;
-//         case "Arcoiris":
-//             total = precioArcoiris;
-//             break;
-//         case "Rompecabezas":
-//             total = precioRompecabezas;
-//             break;
-//         case "Casita":
-//             total = precioCasita;
-//             break;
-//         default:
-//             alert("Ingrese un producto válido o ESC para terminar");
-//             break;
-//     }
-//     console.log(total)
-// }
-
-// console.log(total)
-
-// if (responsableInscripto === "SI" || responsableIncripto === "si" || responsableIncripto === "Si") {
-//     let precioNeto = precio/1.21;
-// }
 
 
-// function totalizar(producto1, producto2, producto3, producto4) {
-//     let total = producto1 + producto2 + producto3 + producto4;
-// }
 
-// totalizar(50,20,30,10);
+alert(`Tu crédito es de $${credito}.  Gracias por tus compras.`)
 
-// console.log(totalizar)
 
-// const total = 0 hacer función
+totalComprado()
+iva()
